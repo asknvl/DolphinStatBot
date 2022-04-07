567cb43a8167371a599f788eca821924d621a8c4
asknvl <asknvl@gmail.com>
2022-04-08 00:33:51 +0300

    +getUsers, testStat

---
+++ DolphinStatBot/DolphinApi/DolphinApi.cs
@@ -0,0 +1,50 @@
+﻿using DolphinStatBot.Users;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DolphinStatBot.DolphinApi
+{
+    public class DolphinApi
+    {
+        #region vars
+        string url;
+        #endregion
+        public DolphinApi(string url)
+        {
+            this.url = url;
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            List<User> users = new List<User>();
+            try
+            {
+                await Task.Run(() => { 
+
+                    var client = new RestClient($"{url}/new/users");
+                    var request = new RestRequest(Method.GET);
+                    request.AddHeader("Authorization", "1-578000f643ac0dd4f72579dd758ebd8e");
+                    request.AddHeader("Content-Type", "application/json");
+                    IRestResponse response = client.Execute(request);                    
+                    JObject json = JObject.Parse(response.Content);
+                    IList<JToken> results = json["data"].Children().ToList();
+                    foreach (JToken result in results)
+                    {
+                        User user = result.ToObject<User>();                        
+                        users.Add(user);
+                    }
+                });
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return users;
+        }
+        
+    }
+}
--- DolphinStatBot/Program.cs
+++ DolphinStatBot/Program.cs
@@ -1,27 +1,38 @@
 ﻿
 
+using DolphinStatBot.DolphinApi;
+
+using DolphinStatBot.Users;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using RestSharp;
 
 class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
-        //var client = new RestClient("https://188.225.43.87/2/#/adsmanager/new/users");        
-        //var request = new RestRequest(Method.GET);
-        //request.AddHeader("Authorization", "1-578000f643ac0dd4f72579dd758ebd8e");
-        //request.AddHeader("Content-Type", "application/json");
-        //var response = client.Execute(request);
-        //Console.WriteLine(response.Content);
+               
+        DolphinApi dolphin = new DolphinApi("http://188.225.43.87");        
+        List<User> users = await dolphin.GetUsers();
 
-        var client = new RestClient("http://188.225.43.87/new/users/");
+
+        var client = new RestClient("http://188.225.43.87/new/stat/by_user?currency=USD");
         client.Timeout = -1;
-        var request = new RestRequest(Method.GET);
+        var request = new RestRequest(Method.POST);
         request.AddHeader("Authorization", "1-578000f643ac0dd4f72579dd758ebd8e");
         request.AddHeader("Content-Type", "application/json");
-       // var body = @"";
-       // request.AddParameter("application/json", body, ParameterType.RequestBody);
+
+        dynamic p = new JObject();
+        p.ids = new JArray();
+        p.ids.Add("0");
+
+        request.AddParameter("application/json", p.ToString(), ParameterType.RequestBody);
+        //request.AddParameter("ids[]", "{1}");
+        request.AddParameter("dates[startDate]", "2022-04-08");
+        request.AddParameter("dates[endDate]", "2022-04-08");
         IRestResponse response = client.Execute(request);
         Console.WriteLine(response.Content);
 
+        Console.ReadLine();
     }
 }
